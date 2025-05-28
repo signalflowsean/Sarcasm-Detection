9a1a58044b7518fa55e3a275638c94dc71d21cec
Sean Nealon <signalflowsean@Seans-MacBook-Pro.local>
2025-05-28 11:13:13 -0400

    basic layout

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import MeterSection from './features/meter'
+
+const App = ()=> {
+
+  return (
+    <main>
+      <section className="primary-spacing">
+        stuff
+      </section>
+      <MeterSection />
+    </main>
+  )
+}
+
+export default App
---
+++ src/features/meter/index.tsx
@@ -0,0 +1,12 @@
+const MeterSection = () => {
+  return (
+    <section className="meter__wrapper">
+      <div className="meter">
+        <div className="meter__display"></div>
+        <div className="meter__controls"></div>  
+      </div>
+    </section>
+  )
+}
+
+export default MeterSection;
