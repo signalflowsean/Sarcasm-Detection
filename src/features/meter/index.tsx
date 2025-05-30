20cae37805cc0f870436663ae3d8eca30a2e7c9d
Sean Nealon <signalflowsean@Seans-MacBook-Pro.local>
2025-05-30 14:28:30 -0400

    meter css: start on display

--- src/features/meter/index.tsx
+++ src/features/meter/index.tsx
@@ -1,11 +1,12 @@
 const MeterSection = () => {
   return (
-    <section className="meter__wrapper">
-      <div className="meter">
-        <div className="meter__display"></div>
-        <div className="meter__controls"></div>  
+    <div className="meter">
+      <div className="meter__display">
+        <div className="meter__display__boundary"></div>
+        <div className="meter__display__boundary"></div>
       </div>
-    </section>
+      <div className="meter__controls"></div>
+    </div>
   )
 }
 
