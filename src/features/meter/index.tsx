2d31342f6c97dbaf74df62ad36e432b3fa93af82
Sean Nealon <signalflowsean@Seans-MacBook-Pro.local>
2025-06-04 21:16:13 -0400

    started on meter level

--- src/features/meter/index.tsx
+++ src/features/meter/index.tsx
@@ -1,13 +1,35 @@
+import type { CSSProperties } from 'react';
+
 const MeterSection = () => {
   return (
     <div className="meter">
       <div className="meter__display">
-        <div className="meter__display__boundary"></div>
-        <div className="meter__display__boundary"></div>
+        <MeterBoundary columnStart={1} rotate="-20deg" />
+        <MeterBoundary columnStart={3} rotate="20deg" />
+        <div className="meter__display__level" />
       </div>
       <div className="meter__controls"></div>
     </div>
   )
+}
+
+type MeterBoundaryProps = {
+  columnStart: number;
+  rotate: string;
+}
+
+const MeterBoundary = ({ columnStart, rotate } : MeterBoundaryProps ) => {
+  return (
+    <div
+      className="meter__display__boundary"
+      style={
+        {
+          "--column-start": columnStart,
+          "--boundary-rotate" : rotate
+        } as CSSProperties
+      }
+    />
+  );
 }
 
 export default MeterSection;
