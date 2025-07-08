20b4aae1702544486a380b5a4a3f3e8d7f411571
Sean Nealon <signalflowsean@macbookpro.mynetworksettings.com>
2025-07-08 16:09:35 -0400

    this is kind of colossal mess but saving here since closest

--- src/features/meter/index.tsx
+++ src/features/meter/index.tsx
@@ -4,8 +4,9 @@ const MeterSection = () => {
   return (
     <div className="meter">
       <div className="meter__display">
-        <MeterBoundary columnStart={1} rotate="-20deg" />
-        <MeterBoundary columnStart={3} rotate="20deg" />
+        <MeterBoundary columnStart={1} rotate="-20deg" className="meter__display__boundary--1" />
+        <MeterBoundary columnStart={3} rotate="20deg" className="meter__display__boundary--2" />
+        <div className="meter__display__level" />
         <div className="meter__display__level" />
       </div>
       <div className="meter__controls"></div>
@@ -16,15 +17,16 @@ const MeterSection = () => {
 type MeterBoundaryProps = {
   columnStart: number;
   rotate: string;
+  className?: string;
 }
 
-const MeterBoundary = ({ columnStart, rotate } : MeterBoundaryProps ) => {
+const MeterBoundary = ({ columnStart, rotate, className } : MeterBoundaryProps ) => {
   return (
     <div
-      className="meter__display__boundary"
+      className={`meter__display__boundary ${className}`}
       style={
         {
-          "--column-start": columnStart,
+          "--column-start" : columnStart,
           "--boundary-rotate" : rotate
         } as CSSProperties
       }
